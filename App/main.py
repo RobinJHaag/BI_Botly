9cad373f9e45fad7d89db824d4690f7898b356d3
Robin Jannes Haag <160750833+RobinJHaag@users.noreply.github.com>
2025-06-15 05:30:35 +0200

    Initial Commit.
    Hätte schon lange mal ein repo erstellen sollen.
    
    DB_side_of_the_Force.py = DB erstellen
    Kamino_Data_Generator.py = Data_Generator
    Dashboard.py = Shiny Dashboard
    
    Ist alles star wars themed falls du das siehst Manuel :)

---
+++ App/main.py
@@ -0,0 +1,8 @@
+from App.DB_side_of_the_Force import dew_it
+from App.Kamino_Data_Generator import lol_fill_stuff
+from App.Dashboard import *
+
+if __name__ == "__main__":
+    dew_it()
+    lol_fill_stuff()
+    app.run()
